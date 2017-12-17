0aec59f2ef47e850d5fee8893747fd21033f0912
Jakob (Koby) Shimony <kobyshi@gmail.com>
2017-12-17 23:39:37 +0100

    Clean up all `normalizeFilePath` function and its tests

--- test/file.test.js
+++ test/file.test.js
@@ -7,7 +7,6 @@ var Sinon  = require('sinon');
 
 var File = require('../lib/file');
 
-var DEFAULT_FILE = './CHANGELOG.md';
 var NONEXISTENT = Path.resolve(__dirname, '../fake-file.txt');
 var README      = Path.resolve(__dirname, '../README.md');
 var STDOUT_FD   = 1;
@@ -75,73 +74,6 @@ describe('file', function () {
       .then(function () {
         Expect(Fs.writeAsync.firstCall.calledWith(STDOUT_FD, data));
         Fs.writeAsync.restore();
-      });
-    });
-
-  });
-
-  describe('normalizePath', function () {
-
-    it('default output, no sub-directory given => CHANGELOG.md in current directory', function () {
-      return File.normalizeFilePath(DEFAULT_FILE)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(DEFAULT_FILE);
-      });
-    });
-
-    it('default name, with sub-directory given => CHANGELOG.md in sub directory', function () {
-      var subDirectory = 'subdirectory';
-
-      return File.normalizeFilePath(DEFAULT_FILE, subDirectory)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(Path.join(subDirectory, DEFAULT_FILE));
-      });
-    });
-
-    it('explicit name, no sub-directory given => explicit name in current directory', function () {
-      var someFile = 'SOMEFILE.md';
-
-      return File.normalizeFilePath(someFile)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(someFile);
-      });
-    });
-
-    it('explicit name, with sub-directory given => explicit name in sub directory', function () {
-      var subDirectory = 'subdirectory';
-      var someFile = 'SOMEFILE.md';
-
-      return File.normalizeFilePath(someFile, subDirectory)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(Path.join(subDirectory, someFile));
-      });
-    });
-
-    it('default name, with deep-sub-directory given => CHANGELOG.md in deep-sub directory', function () {
-      var subDirectory = Path.join('path', 'to', 'subdirectory');
-
-      return File.normalizeFilePath(DEFAULT_FILE, subDirectory)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(Path.join(subDirectory, DEFAULT_FILE));
-      });
-    });
-
-    it('explicit output path, without sub-directory given => explicit output path', function () {
-      var explicitPath = Path.join('path', 'to', DEFAULT_FILE);
-
-      return File.normalizeFilePath(explicitPath)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(explicitPath);
-      });
-    });
-
-    it('explicit output path, with sub-directory given => explicit output path', function () {
-      var explicitPath = Path.join('path', 'to', DEFAULT_FILE);
-      var subDirectory = Path.join('some', 'other', 'directory');
-
-      return File.normalizeFilePath(explicitPath, subDirectory)
-      .then(function (normalizedPath) {
-        Expect(normalizedPath).to.eql(explicitPath);
       });
     });
 
